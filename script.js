0777c798759e2f6f4a6d9a15958337ab34e549bd
Lauren Stuart <lauren.prueter@gmail.com>
2023-11-16 17:57:01 -0500

    Tweaked button

--- script.js
+++ script.js
@@ -9,16 +9,26 @@ button.addEventListener('click', fetchJoke);
 async function fetchJoke() {
     const jokeData = await fetch('https://icanhazdadjoke.com/', {
         headers: {
-            'Accept': 'application/json', 
+            'Accept': 'application/json',
             "User-Agent": "LoloStuarts Library"
         }
     });
-const jokeObj = await jokeData.json();
-jokeText.innerHTML = jokeObj.joke;
+    const jokeObj = await jokeData.json();
+    jokeText.innerHTML = jokeObj.joke;
 }
 
-// function hover() {
-//     let btn= document.querySelector('.projects-btn')
-//     btn.classList.add ('hover')
-//     console.log(hover);
-// }
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "lauren.prueter@gmail.com",
+        Password: "password",
+        To: 'lauren.prueter@gmail.com',
+        From: document.getElementById("email").value,
+        Subject: "New message from website",
+        Body: "Name: " + document.getElementById("name").value
+        + "<br> Email: " + document.getElementById("email").value
+        + "<br> Message: " + document.getElementById("message").value
+    }).then(
+        message => alert("Message sent. Thank you.")
+    );
+}
