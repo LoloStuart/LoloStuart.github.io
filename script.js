121f22d875cd565e9f6ea87c331a1cc0e885ecdd
Lauren Stuart <lauren.prueter@gmail.com>
2023-11-16 14:36:37 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Help from https://www.youtube.com/watch?v=UDIfuvLEkjU
+
+const button = document.querySelector('.jokeBox button');
+const jokeText = document.querySelector('.jokeBox p');
+
+
+button.addEventListener('click', fetchJoke);
+
+async function fetchJoke() {
+    const jokeData = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json', 
+            "User-Agent": "LoloStuarts Library"
+        }
+    });
+const jokeObj = await jokeData.json();
+jokeText.innerHTML = jokeObj.joke;
+}
